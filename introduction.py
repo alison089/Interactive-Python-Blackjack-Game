e3adb41160954c8cef733530bed0914f2b4363f9
alison089 <148564064+alison089@users.noreply.github.com>
2023-12-02 23:01:53 -0500

    introduction.py

--- introduction.py
+++ introduction.py
@@ -4,11 +4,17 @@ print("Welcome to our Game of Blackjack!")
 name = input("What is your name? ") 
 print("Hello " + name + "!")
 
-choice = input("Are you ready to play? (Y/N) : ").lower()
-if choice == "y":
-    print("Lets get started " + name + "!")
-else: 
-    print("Entry must be either Y or N")
-    print("No worries, bye bye 👋!")
+while True:
+    try:
+        choice = int(input("Are you ready to play? (1: Y/ 2: N) : "))
+        assert (int(choice) < 1 )
+        assert(int(choice) > 2)
+        break
+    except ValueError:
+        print("please enter a number")
+    except:
+        ("please enter 1: Yes or 2: No")
+    break
+if choice == 2:
+    print("Oh No! Have a good one! Bye Bye!")
     exit()
-    
